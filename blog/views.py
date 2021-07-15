60d37b7d52a5d672ec186ec70f6e99e382b7442d
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2021-07-15 17:38:35 -0400

    added special template features & syntax, added a single post page and a 404 page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,84 @@
+from datetime import date 
+
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "MichiganHiking300.jpg",
+        "author": "Russ Wheeler",
+        "date": date(2021, 7, 1),
+        "title": "Kentucky Hiking", 
+        "excerpt": "There's nothing like the views you get when hiking in Kentucky!", 
+        "content": """ 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsa soluta cumque illo eligendi illum maxime necessitatibus
+            explicabo neque facere, perferendis dolorum quas molestiae, asperiores eaque ea, beatae minus. Ad? 
+            """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "skylineComputer.jpg",
+        "author": "Russ Wheeler",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "hiking.jpg",
+        "author": "Russ Wheeler",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+     "all_posts": all_posts
+     })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
 
