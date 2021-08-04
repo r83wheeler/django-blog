c3d4ae5e63bac39e977f5a0f160c497559f8eda1
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2021-08-04 11:30:02 -0400

    using author & tags data

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
